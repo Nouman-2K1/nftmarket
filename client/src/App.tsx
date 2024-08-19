76224b5eb56cc310bea58a941b4d3cfedf0a15bb
Muhammad Nouman <m.nouman.nomi@gmail.com>
2024-08-19 20:09:57 +0500

    implement navbar with matamask wallet connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import MarketPlace from "./modules/Marketplace";
+import CreateNFT from "./modules/NFTMinting";
+import Profile from "./modules/Profile";
 
-
-function App() { 
-
+function App() {
   return (
-    <>
-      <div className='text-3xl font-bold underline'>
-        hello
-        </div>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/main/marketplace" />} />
+      <Route path="/main" element={<MainLayout />}>
+        <Route path="marketplace" element={<MarketPlace />} />
+        <Route path="create-nft" element={<CreateNFT />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/modules/Marketplace.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MarketPlace: React.FC = () => {
+  return <div>MarketPlace Page</div>;
+};
+
+export default MarketPlace;
